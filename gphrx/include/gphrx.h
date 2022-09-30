e33538af2d26f72498640b5c609180c2e1f6540b
Tanner Davies <tanner.e.davies@gmail.com>
2022-09-30 09:47:05 -0600

    Slight modification to plans and interface for graph

--- gphrx/include/gphrx.h
+++ gphrx/include/gphrx.h
@@ -1,5 +1,6 @@
 #ifndef __GPHRX_H
 
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "assert.h"
@@ -7,9 +8,9 @@
 #include "dynarray.h"
 #include "intrinsics.h"
 
-typedef u8 GPHRX_ERROR;
-#define GPHRX_NO_ERROR 0
-#define GPHRX_FILE_IO_FAILURE 1
+// TODO: Create separate struct and functions for weighted graphs. The CSR adjacency matrix will
+//       be fundamentally different for weighted graphs functions for adding vertices and edges
+//       will accept a weight as a parameter
 
 /**
  * Compress Space Row formatted adjacency matrix stored with dynamic arrays
@@ -23,30 +24,49 @@ typedef struct {
  * Metadata and representation of a graph
  */
 typedef struct {
+    bool is_directed;
     u64 node_count;
     CsrAdjMatrix adjacency_matrix;
 } GphrxGraph;
 
 /**
- * Creates an empty GraphRox graph.
+ * Creates an empty undirected GraphRox graph.
  */
-GphrxGraph new_gphrx();
+GphrxGraph new_undirected_gphrx();
 
 /**
- * Generates an approximation of a graph at a given depth in the bit-order tree representation of the graph.
- * This is where the magic of GraphRox happens.
+ * Creates an empty directed GraphRox graph.
  */
-GphrxGraph approximate_gphrx(GphrxGraph *graph, u64 depth);
+GphrxGraph new_directed_gphrx();
 
 /**
- * Loads a graph from a GraphRox file
+ * Add a vertex to the given graph.
  */
-GphrxGraph gphrx_load_from_file(char *file_path);
+void gphrx_add_vertex(GphrxGraph *graph, u64 *adjacent_vertices);
 
 /**
- * Saves a GraphRox graph to a file
+ * Removes a vertex from the given graph.
  */
-void gphrx_save(GphrxGraph *graph, char *file_path);
+void gphrx_remove_vertex(GphrxGraph *graph, u64 vertex_id);
+
+/**
+ * Adds a link between two vertices. The "from" and "to" qualifiers on parameter names are only significant
+ * when the graph is directed.
+ */
+void gphrx_add_edge(GphrxGraph *graph, u64 from_vertex_id, u64 to_vertex_id);
+
+/**
+ * Removes a link between two vertices. The "from" and "to" qualifiers on parameter names are only
+ * significant when the graph is directed.
+ */
+void gphrx_delete_edge(GphrxGraph *graph, u64 from_vertex_id, u64 to_vertex_id);
+
+/**
+ * Generates an approximation of a graph at a given depth in the bit-order tree representation of the graph
+ * and a threshold of what percentage of edges must exist in a segment of the adjacency matrix for the
+ * segment to be represented in the approximation. This is where the magic of GraphRox happens.
+ */
+GphrxGraph approximate_gphrx(GphrxGraph *graph, u64 depth, float threshold);
 
 #define __GPHRX_H
 #endif
--- gphrx/src/gphrx.c
+++ gphrx/src/gphrx.c
@@ -1,6 +1,6 @@
 #include "gphrx.h"
 
-GphrxGraph new_gphrx()
+static GphrxGraph new_gphrx(bool is_directed)
 {
     CsrAdjMatrix adjacency_matrix = {
         .matrix_col_idx_list = new_dynarr(u8),
@@ -8,9 +8,38 @@ GphrxGraph new_gphrx()
     };
     
     GphrxGraph graph = {
+        .is_directed = is_directed,
         .node_count = 0,
         .adjacency_matrix = adjacency_matrix,
     };
 
     return graph;
 }
+
+GphrxGraph new_undirected_gphrx()
+{
+    return new_gphrx(false);
+}
+
+GphrxGraph new_directed_gphrx()
+{
+    return new_gphrx(true);
+}
+
+void gphrx_add_vertex(GphrxGraph *graph, u64 *adjacent_vertices)
+{
+    // TODO: Insert row into adjacency_matrix for new vertex
+    // TODO: Loop over adjacent_vertices. For new row in matrix, place 1 per adjacent vertex. If
+    //       graph is undirected, also add 1 to adjacent matricies' rows. If a provided adjacent
+    //       vertex doesn't exist, create it by extending the matrix out
+
+    ++graph->node_count;
+}
+
+void gphrx_remove_vertex(GphrxGraph *graph, u64 vertex_id);
+
+void gphrx_add_edge(GphrxGraph *graph, u64 from_vertex_id, u64 to_vertex_id);
+
+void gphrx_delete_edge(GphrxGraph *graph, u64 from_vertex_id, u64 to_vertex_id);
+
+GphrxGraph approximate_gphrx(GphrxGraph *graph, u64 depth, float threshold);
