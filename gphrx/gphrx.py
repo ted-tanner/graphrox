b056a60b4b27796ed2f6da4778470e3d703fe171
Tanner Davies <tanner.e.davies@gmail.com>
2022-10-10 08:45:59 -0600

    Add all function to Python library

--- gphrx/gphrx.py
+++ gphrx/gphrx.py
@@ -5,91 +5,86 @@ import os
 import platform
 
 
-__libs_path = os.path.join(Path(__file__).resolve().parent, 'lib')
+_libs_path = os.path.join(Path(__file__).resolve().parent, 'lib')
 
-__os_name = platform.system()
+_os_name = platform.system()
 
-if __os_name == 'Linux':
-    __dll_name = 'graphrox.so'
-elif __os_name == 'Windows':
-    __dll_name = 'graphrox.dll'
-elif __os_name == 'Darwin':
+if _os_name == 'Linux':
+    _dll_name = 'graphrox.so'
+elif _os_name == 'Windows':
+    _dll_name = 'graphrox.dll'
+elif _os_name == 'Darwin':
     if 'x86_64' == platform.uname().machine:
-        __dll_name = 'graphrox-x86.dylib'
+        _dll_name = 'graphrox-x86.dylib'
     else:
-        __dll_name = 'graphrox-arm64.dylib'
+        _dll_name = 'graphrox-arm64.dylib'
 else:
     raise ImportError('GraphRox module not supported on this system')
 
 
-class DynamicArrayU64(ctypes.Structure):
+_gphrx_lib_path = os.path.join(_libs_path, _dll_name)
+_gphrx_lib = ctypes.cdll.LoadLibrary(_gphrx_lib_path)
+
+
+class _DynamicArrayU64(ctypes.Structure):
     _fields_ = [
         ("capacity", ctypes.c_size_t),
         ("size", ctypes.c_size_t),
         ("arr", ctypes.POINTER(ctypes.c_uint64))]
-
-class CsrAdjMatrix(ctypes.Structure):
+    
+class _CsrAdjMatrix(ctypes.Structure):
     _fields_ = [
-        ("matrix_col_idx_list", DynamicArrayU64),
-        ("matrix_row_idx_list", DynamicArrayU64)]
+        ("matrix_col_idx_list", _DynamicArrayU64),
+        ("matrix_row_idx_list", _DynamicArrayU64)]
 
-class GphrxGraph(ctypes.Structure):
+class _GphrxGraph(ctypes.Structure):
     _fields_ = [
         ("is_undirected", ctypes.c_bool),
         ("highest_vertex_id", ctypes.c_uint64),
-        ("adjacency_matrix", CsrAdjMatrix)]
-    
+        ("adjacency_matrix", _CsrAdjMatrix)]
 
-__gphrx_lib_path = os.path.join(__libs_path, __dll_name)
-__gphrx_lib = ctypes.cdll.LoadLibrary(__gphrx_lib_path)
+_gphrx_lib_path = os.path.join(_libs_path, _dll_name)
+_gphrx_lib = ctypes.cdll.LoadLibrary(_gphrx_lib_path)
 
-__gphrx_lib.new_undirected_gphrx.argtypes = None
-__gphrx_lib.new_undirected_gphrx.restype = GphrxGraph
+_gphrx_lib.new_undirected_gphrx.argtypes = None
+_gphrx_lib.new_undirected_gphrx.restype = _GphrxGraph
 
-__gphrx_lib.free_gphrx.argtypes = [ctypes.POINTER(GphrxGraph)]
-__gphrx_lib.free_gphrx.restype = None
+_gphrx_lib.new_directed_gphrx.argtypes = None
+_gphrx_lib.new_directed_gphrx.restype = _GphrxGraph
 
-test = __gphrx_lib.new_undirected_gphrx()
+_gphrx_lib.free_gphrx.argtypes = [ctypes.POINTER(_GphrxGraph)]
+_gphrx_lib.free_gphrx.restype = None
+
+_gphrx_lib.gphrx_shrink.argtypes = [ctypes.POINTER(_GphrxGraph)]
+_gphrx_lib.gphrx_shrink.restype = None
+
+_gphrx_lib.gphrx_add_vertex.argtypes = (ctypes.POINTER(_GphrxGraph),
+                                         ctypes.c_uint64,
+                                         ctypes.POINTER(ctypes.c_uint64),
+                                         ctypes.c_uint64)
+_gphrx_lib.gphrx_add_vertex.restype = None
+
+_gphrx_lib.gphrx_remove_vertex.argtypes = (ctypes.POINTER(_GphrxGraph), ctypes.c_uint64)
+_gphrx_lib.gphrx_remove_vertex.restype = None
+
+_gphrx_lib.gphrx_add_edge.argtypes = (ctypes.POINTER(_GphrxGraph), ctypes.c_uint64, ctypes.c_uint64)
+_gphrx_lib.gphrx_add_edge.restype = None
+
+_gphrx_lib.gphrx_remove_edge.argtypes = (ctypes.POINTER(_GphrxGraph), ctypes.c_uint64, ctypes.c_uint64)
+_gphrx_lib.gphrx_remove_edge.restype = ctypes.c_uint8
+
+_gphrx_lib.gphrx_to_byte_array.argtypes = (ctypes.POINTER(_GphrxGraph), ctypes.c_uint64, ctypes.c_uint64)
+_gphrx_lib.gphrx_to_byte_array.restype = ctypes.POINTER(ctypes.c_ubyte)
+
+_gphrx_lib.gphrx_to_byte_array.argtypes = (ctypes.POINTER(ctypes.c_ubyte), ctypes.POINTER(ctypes.c_uint8))
+_gphrx_lib.gphrx_to_byte_array.restype = _GphrxGraph
+
+
+
+
+
+test = _gphrx_lib.new_undirected_gphrx()
 
 print(test)
 
-__gphrx_lib.free_gphrx(test)
-
-
-
-# __gphrx_lib.csv_to_json.argtypes = (ctypes.c_char_p, ctypes.c_size_t)
-# __gphrx_lib.csv_to_json.restype = ctypes.c_void_p
-
-# __gphrx_lib.free_json.argtypes = [ctypes.c_void_p]
-# __gphrx_lib.free_json.restype = None
-
-
-# class __DynamicArray64(ctypes.Structure):
-#     _fields_ = [('capacity', ctypes.c_size_t),
-#                 ('size', ctypes.c_size_t),
-#                 ('arr', c_void_p)]
-
-    
-# class __CsrAdjMatrix(ctypes.Structure):
-#     _fields_ = [('matrix_col_idx_list', c_int),
-#                 ('matrix_row_idx_list', c_double)]
-    
-
-# class GphrxGraph(ctypes.Structure):
-#     _fields_ = [('num', c_int),
-#                 ('dnum', c_double)]
-
-
-# def csv_to_json(csv, as_str=False):
-#     if isinstance(csv, str):
-#         csv = bytes(csv, encoding='utf-8')
-
-#     ptr = __c_csv_lib.csv_to_json(csv, len(csv))
-#     json = ctypes.cast(ptr, ctypes.c_char_p).value
-
-#     __c_csv_lib.free_json(ptr)
-
-#     if as_str:
-#         return json.decode('utf-8')
-#     else:
-#         return json
+_gphrx_lib.free_gphrx(test)
